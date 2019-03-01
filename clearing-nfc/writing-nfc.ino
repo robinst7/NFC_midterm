5c3c0407315b6046ad2b72205eed63ca1e7be7fd
robinst7 <45833549+robinst7@users.noreply.github.com>
2019-02-28 20:37:03 -0500

    Add files via upload

---
+++ clearing-nfc/writing-nfc.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>
+#include <PN532_I2C.h>
+#include <PN532.h>   // The following files are included in the libraries Installed
+#include <NfcAdapter.h>
+
+PN532_I2C pn532_i2c(Wire);
+NfcAdapter nfc = NfcAdapter(pn532_i2c);  // Indicates the Shield you are using
+
+void setup() {
+   Serial.begin(9600);
+    Serial.println("NFC Tag Writer"); // Serial Monitor Message
+    nfc.begin();
+}
+
+void loop() {
+  Serial.println("\nPlace an NFC Tag that you want to Record these Messages on!"); // Command for the Serial Monitor
+    if (nfc.tagPresent()) {
+        NdefMessage message = NdefMessage();
+        message.addTextRecord("Hi! Here is a link to my gitHub"); // You can put a text message in the quotes here
+        message.addUriRecord("https://github.com/robinst7/NFC_midterm"); //You can put a url here if you want as well
+        boolean success = nfc.write(message);
+        if (success) {
+            Serial.println("Good Job, now read it with your phone!"); // if it works you will see this message 
+        } else {
+            Serial.println("Write failed"); // If the the rewrite failed you will see this message
+        }
+    }
+    delay(10000);
+
+}
